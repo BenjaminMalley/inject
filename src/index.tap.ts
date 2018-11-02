0993dcc03b8ddf8b79e6aae292dee45dec077c65
Benjamin Malley <benjamin.malley@uber.com>
2018-11-01 19:46:11 -0700

    add prettier

--- src/index.tap.ts
+++ src/index.tap.ts
@@ -3,15 +3,14 @@ import ProgramLoader from "./index"
 import * as test from "tape"
 import * as path from "path"
 
-
 test("ProgramLoader#getProviders should return all of the providers from a valid ts program", assert => {
-  const fixturePath = path.join(__dirname, "..", "fixtures", "foo.ts")
-  const program = ts.createProgram([fixturePath], {
-    target: ts.ScriptTarget.ES2015,
-    module: ts.ModuleKind.CommonJS,
-  })
-  const loader = new ProgramLoader(program)
-  const providers = loader.getProviders()
-  assert.equal(providers.size, 5)
-  assert.end()
+    const fixturePath = path.join(__dirname, "..", "fixtures", "foo.ts")
+    const program = ts.createProgram([fixturePath], {
+        target: ts.ScriptTarget.ES2015,
+        module: ts.ModuleKind.CommonJS,
+    })
+    const loader = new ProgramLoader(program)
+    const providers = loader.getProviders()
+    assert.equal(providers.size, 5)
+    assert.end()
 })
